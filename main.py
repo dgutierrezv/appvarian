6a6351b4fc148cee1b345f173d5499ca88fbae84
Danny Gutiérrez Vargas <dgutierrez@promedpa.local>
2024-07-24 12:25:38 -0600

    COMMIT V1.9.3.1

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ from fastapi.templating import Jinja2Templates
 from motor.motor_asyncio import AsyncIOMotorClient
 from bson import ObjectId
 from starlette.middleware.sessions import SessionMiddleware
-from fastapi.staticfiles import StaticFiles
+from passlib.context import CryptContext
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
 app.add_middleware(SessionMiddleware, secret_key="your_secret_key")
 
 templates = Jinja2Templates(directory="templates")
@@ -23,24 +22,27 @@ client = AsyncIOMotorClient("mongodb+srv://dgutierrez:ExMv2hxPlRFbxO5F@cluster0.
 db = client.stock_db
 collection = db.parts
 
+# Password hashing
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 fake_users_db = {
     "varian": {
         "username": "varian",
         "full_name": "Varian User",
         "email": "varian@example.com",
-        "hashed_password": "1e$civres",
+        "hashed_password": pwd_context.hash("1e$civres"),
         "disabled": False,
     }
 }
 
-def fake_hash_password(password: str):
-    return password
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
 
 def authenticate_user(fake_db, username: str, password: str):
     user = fake_db.get(username)
     if not user:
         return False
-    if not fake_hash_password(password) == user["hashed_password"]:
+    if not verify_password(password, user["hashed_password"]):
         return False
     return user
 
@@ -72,10 +74,10 @@ async def read_login(request: Request):
 @app.post("/login", response_class=HTMLResponse)
 async def login(request: Request, username: str = Form(...), password: str = Form(...)):
     if not username or not password:
-        return templates.TemplateResponse("login.html", {"request": request, "error": "Both fields are required"})
+        return templates.TemplateResponse("login.html", {"request": request, "error": "Ambos campos son obligatorios"})
     user = authenticate_user(fake_users_db, username, password)
     if not user:
-        return templates.TemplateResponse("login.html", {"request": request, "error": "Invalid credentials"})
+        return templates.TemplateResponse("login.html", {"request": request, "error": "Credenciales inválidos"})
     request.session["user"] = username
     return RedirectResponse(url="/stock", status_code=302)
 
@@ -87,14 +89,30 @@ async def read_stock(request: Request):
 
 @app.post("/add_part", response_class=HTMLResponse)
 async def add_part(request: Request, part_number: str = Form(...), description: str = Form(...), quantity: int = Form(...), warehouse: str = Form(...)):
+    existing_part = await collection.find_one({"part_number": part_number, "warehouse": warehouse})
+    if existing_part:
+        
+        parts = await collection.find(existing_part).to_list(100)
+        return templates.TemplateResponse("stock.html", {"request": request, "parts": parts, "error": "Número de parte ya existe en la bodega"})
+    
     part = {"part_number": part_number, "description": description, "quantity": quantity, "warehouse": warehouse}
     await collection.insert_one(part)
-    return RedirectResponse(url="/stock", status_code=302)
+    parts = await collection.find(part).to_list(100)
+    return templates.TemplateResponse("stock.html", {"request": request, "parts": parts, "success_add": "Parte agregada correctamente"})
 
 @app.post("/update_part", response_class=HTMLResponse)
 async def update_part(request: Request, id: str = Form(...), part_number: str = Form(...), description: str = Form(...), quantity: int = Form(...), warehouse: str = Form(...)):
     await collection.update_one({"_id": ObjectId(id)}, {"$set": {"part_number": part_number, "description": description, "quantity": quantity, "warehouse": warehouse}})
-    return RedirectResponse(url=f"/search?query={part_number}", status_code=302)
+    parts = await collection.find({"part_number": part_number, "description": description, "quantity": quantity, "warehouse": warehouse}).to_list(100)
+    return templates.TemplateResponse("stock.html", {"request": request, "parts": parts, "success_update": "Parte actualizada correctamente"})
+
+@app.post("/delete_part", response_class=HTMLResponse)
+async def delete_part(request: Request, id: str = Form(...)):
+    result = await collection.delete_one({"_id": ObjectId(id)})
+    if result.deleted_count == 0:
+        raise HTTPException(status_code=404, detail="Part not found")
+    parts = await collection.find().to_list(100)
+    return templates.TemplateResponse("stock.html", {"request": request, "success_delete": "Parte eliminada correctamente"})
 
 @app.get("/search", response_class=HTMLResponse)
 @app.post("/search", response_class=HTMLResponse)
@@ -108,7 +126,14 @@ async def search(request: Request, query: str = Query(None), form_query: str = F
             {"warehouse": {"$regex": query, "$options": "i"}}
         ]
     }).to_list(100)
-    return templates.TemplateResponse("stock.html", {"request": request, "parts": parts})
+    if len(parts) != 0:
+         return templates.TemplateResponse("stock.html", {"request": request, "parts": parts})
+    else:
+        return templates.TemplateResponse("stock.html", {"request": request, "found": "Parte o bodéga no encontrada"})
+   
+@app.get("/clean", response_class=HTMLResponse)
+async def clean(request: Request):
+    return templates.TemplateResponse("stock.html", {"request": request})
 
 @app.get("/logout", response_class=HTMLResponse)
 async def logout(request: Request):
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const deleteForms = document.querySelectorAll('.delete-form');
+    const scannerModal = document.getElementById('scanner-modal');
+    const scannerContainer = document.querySelector('.scanner-container');
+    const closeScannerButton = document.getElementById('close-scanner');
 
     deleteForms.forEach(form => {
         form.addEventListener('submit', function(event) {
-            const confirmed = confirm('Esta seguro de querer eliminar esta parte!?');
+            const confirmed = confirm('¿Está seguro de querer borrar esta parte?');
             if (!confirmed) {
                 event.preventDefault();
             }
@@ -11,30 +14,41 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
 
     function startScanner(callback) {
-        document.getElementById('scanner-modal').style.display = 'flex';
+        scannerModal.style.display = 'flex';
+
+        // Ajustar el tamaño del contenedor de la cámara
+        scannerContainer.style.width = '300px';  // Ajusta el ancho según tus necesidades
+        scannerContainer.style.height = '200px'; // Ajusta la altura según tus necesidades
+
         Quagga.init({
-            inputStream : {
-                name : "Live",
-                type : "LiveStream",
-                target: document.querySelector('#scanner')
+            inputStream: {
+                type: 'LiveStream',
+                target: scannerContainer,
+                constraints: {
+                    facingMode: 'environment'
+                }
             },
-            decoder : {
-                readers : ["code_128_reader"]
+            decoder: {
+                readers: ['code_128_reader']
             }
         }, function(err) {
             if (err) {
-                console.log(err);
+                console.error(err);
                 return;
             }
-            console.log("Initialization finished. Ready to start");
             Quagga.start();
         });
 
-        Quagga.onDetected(function(data) {
-            callback(data.codeResult.code);
-            Quagga.stop();
-            document.getElementById('scanner-modal').style.display = 'none';
+        Quagga.onDetected(function(result) {
+            const code = result.codeResult.code;
+            callback(code);
+            stopScanner();
         });
+    }
+
+    function stopScanner() {
+        scannerModal.style.display = 'none';
+        Quagga.stop();
     }
 
     document.getElementById('start-scan').addEventListener('click', function() {
@@ -46,6 +60,41 @@ document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('start-scan-search').addEventListener('click', function() {
         startScanner(function(code) {
             document.getElementById('search_query').value = code;
+        });
+    });
+
+    closeScannerButton.addEventListener('click', stopScanner);
+
+    const updateForms = document.querySelectorAll('.update-form');
+    updateForms.forEach(form => {
+        const inputs = form.querySelectorAll('input');
+        const submitButton = form.querySelector('button.actualizar-button');
+        const cancelButton = form.querySelector('.cancel-button');
+        let originalValues = {};
+
+        inputs.forEach(input => {
+            originalValues[input.name] = input.value;
+            input.addEventListener('input', () => {
+                submitButton.style.display = 'inline-block';
+                submitButton.disabled = false;
+                cancelButton.style.display = 'inline-block';
+            });
+        });
+
+        form.addEventListener('submit', function(event) {
+            const confirmed = confirm('¿Estás seguro de que deseas realizar los cambios?');
+            if (!confirmed) {
+                event.preventDefault();
+            }
+        });
+
+        cancelButton.addEventListener('click', () => {
+            inputs.forEach(input => {
+                input.value = originalValues[input.name];
+            });
+            submitButton.style.display = 'none';
+            submitButton.disabled = true;
+            cancelButton.style.display = 'none';
         });
     });
 });
